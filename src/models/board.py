251d458e3369c736c5fb2da7a153eca7c5083ede
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-10-18 16:39:36 -0400

    Added docstrings.

--- src/models/board.py
+++ src/models/board.py
@@ -3,15 +3,18 @@
 from constants.constants import CellValue, BOARD_SIZE
 
 
+BLANK_BOARD: list[list[CellValue]] = [
+    [None for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)
+]
+
+
 class Board:
     """Game board."""
 
     _board: list[list[CellValue]]
 
-    def __init__(self) -> None:
-        self._board: list[list[CellValue]] = [
-            [None for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)
-        ]
+    def __init__(self, board_state: list[list[CellValue]] = BLANK_BOARD) -> None:
+        self._board: list[list[CellValue]] = board_state
 
     def __str__(self) -> str:
         return "Board"
@@ -19,6 +22,15 @@ class Board:
     def __repr__(self) -> str:
         return "Board()"
 
+    def _is_valid_cell_location(self, row: int, col: int) -> bool:
+        return 0 <= row <= BOARD_SIZE and 0 <= col <= BOARD_SIZE
+
     def get_board(self) -> list[list[CellValue]]:
+        """Get a copy of the game board state.
+
+        Returns:
+            list[list[CellValue]]: A copy of the game board state.
+        """
+
         board_copy: list[list[CellValue]] = [row[:] for row in self._board]
         return board_copy
--- tests/models/test_board.py
+++ tests/models/test_board.py
@@ -7,11 +7,14 @@ from constants.constants import CellValue, BOARD_SIZE
 
 
 class TestBoardGetBoard(unittest.TestCase):
+    """test Board.get_board implementation."""
 
-    def setUp(self):
+    def setUp(self) -> None:
         self.board = Board()
 
     def test_get_board_returns_correctly_sized_board(self) -> None:
+        """Test that the Board.get_board returns a board of the correct dimensions."""
+
         board: list[list[CellValue]] = self.board.get_board()
 
         self.assertEqual(len(board), BOARD_SIZE)
