6ea5550746d762e00331f9d24fa5e6c2841ff09b
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-10-18 20:07:51 -0400

    Implemented colorize().

--- src/utils/colorize.py
+++ src/utils/colorize.py
@@ -1,21 +1,47 @@
-"""Turn text grey."""
+"""Colorize text."""
+
+from enum import Enum, auto
 
 from constants.constants import ANSI_GREY, ANSI_RED, ANSI_GREEN, ANSI_RESET
+
+
+class Color(Enum):
+    """Color option enum for colorize()."""
+
+    GREY = auto()
+    RED = auto()
+    GREEN = auto()
+
+
+def colorize(input_str: str, color: Color) -> str:
+    """Colorize input text."""
+
+    match color:
+        case Color.GREY:
+            colorized_str: str = f"{ANSI_GREY}{input_str}"
+        case Color.RED:
+            colorized_str = f"{ANSI_RED}{input_str}"
+        case Color.GREEN:
+            colorized_str = f"{ANSI_GREEN}{input_str}"
+        case _:
+            raise ValueError(f"Invalid color: {color!r}.")
+
+    return f"{colorized_str}{ANSI_RESET}"
 
 
 def grey(input_str: str) -> str:
     """Turn text grey."""
 
-    return f"{ANSI_GREY}{input_str}{ANSI_RESET}"
+    return colorize(input_str, Color.GREY)
 
 
 def red(input_str: str) -> str:
     """Turn text red."""
 
-    return f"{ANSI_RED}{input_str}{ANSI_RESET}"
+    return colorize(input_str, Color.RED)
 
 
 def green(input_str: str) -> str:
     """Turn text green."""
 
-    return f"{ANSI_GREEN}{input_str}{ANSI_RESET}"
+    return colorize(input_str, Color.GREEN)
