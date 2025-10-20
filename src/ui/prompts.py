e5e33df0dfc18293c0d1df7c0ec4ad7271db848f
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-10-20 13:50:35 -0400

    Added prompts.

---
+++ src/ui/prompts.py
@@ -0,0 +1,61 @@
+"""Docstring goes here."""
+
+from typing import cast
+
+from constants.constants import (
+    PlayerMarker,
+    CellKey,
+    Cell,
+    VALID_CELL_KEYS,
+    CELL_KEY_TO_CELL_MAP,
+)
+
+from src.models.board import Board, InvalidCellError, InvalidMoveError
+
+from src.utils.colorize import red, green, cyan
+
+
+class InvalidCellKeyError(Exception):
+    """_summary_
+
+    Args:
+        Exception (_type_): _description_
+    """
+
+    _message: str
+
+    def __init__(self, message: str = "Invalid cell key.") -> None:
+        self._message: str = message
+        super().__init__(self._message)
+
+
+def _get_cell_from_cell_key(cell_key: CellKey) -> Cell:
+    if not cell_key in VALID_CELL_KEYS:
+        raise InvalidCellKeyError(f"Invalid cell key: {cell_key!r}.")
+
+    cell: Cell = CELL_KEY_TO_CELL_MAP[cell_key]
+    return cell
+
+
+def prompt(player_marker: PlayerMarker, board: Board) -> None:
+    """_summary_
+
+    Args:
+        player_marker (PlayerMarker): _description_
+        board (Board): _description_
+    """
+
+    player_str = red("X") if player_marker == "X" else green("O")
+    player_prompt: str = f"\n   {player_str} {cyan('→')} "
+
+    while True:
+        print(f"\n{board.stringify_board()}")
+        move: str = input(player_prompt)
+
+        try:
+            cell = _get_cell_from_cell_key(cast(CellKey, move))
+            board.make_move(cell, player_marker)
+            break
+        except (InvalidCellKeyError, InvalidCellError, InvalidMoveError) as _:
+            print(red(" Try again "))
+            continue
